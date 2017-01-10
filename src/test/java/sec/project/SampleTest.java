b818601d202f0f5abd8d070249e733f5fa9015aa
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-10 20:12:59 +0200

    `public void dummyTest() throws Throwable`. A dummy test w/o assertions.
    	modified:   src/test/java/sec/project/SampleTest.java

--- src/test/java/sec/project/SampleTest.java
+++ src/test/java/sec/project/SampleTest.java
@@ -1,21 +1,14 @@
 package sec.project;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import sec.project.repository.SignupRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,9 +16,6 @@ public class SampleTest {
 
     @Autowired
     private WebApplicationContext webAppContext;
-
-    @Autowired
-    private SignupRepository signupRepository;
 
     private MockMvc mockMvc;
 
@@ -35,8 +25,6 @@ public class SampleTest {
     }
 
     @Test
-    public void signupAddsDataToDatabase() throws Throwable {
-        mockMvc.perform(post("/form").param("name", "Testname").param("address", "Testaddress")).andReturn();
-        assertEquals(1L, signupRepository.findAll().stream().filter(s -> s.getName().equals("Testname") && s.getAddress().equals("Testaddress")).count());
+    public void dummyTest() throws Throwable {
     }
 }
