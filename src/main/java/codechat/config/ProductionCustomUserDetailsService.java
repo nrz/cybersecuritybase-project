3f2118b3d589df8dfb9f5363dcf2ce1a2789b435
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-12 22:56:05 +0200

    new class `ProductionCustomUserDetailsService`. `@Profile("production")`
    	new file:   src/main/java/codechat/config/ProductionCustomUserDetailsService.java

---
+++ src/main/java/codechat/config/ProductionCustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package codechat.config;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Profile("production")
+@Service
+public class ProductionCustomUserDetailsService implements UserDetailsService {
+
+    private Map<String, String> accountDetails;
+
+    @PostConstruct
+    public void init() {
+        // this data would typically be retrieved from a database
+        this.accountDetails = new TreeMap<>();
+        this.accountDetails.put("ted", "$2a$06$rtacOjuBuSlhnqMO2GKxW.Bs8J6KI0kYjw/gtF0bfErYgFyNTZRDm");
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username == null) {
+            throw new UsernameNotFoundException("username must not be null!");
+        }
+
+        if (username.isEmpty()) {
+            throw new UsernameNotFoundException("username must not be an empty string!");
+        }
+
+        if (!this.accountDetails.containsKey(username)) {
+            throw new UsernameNotFoundException("No such user: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                this.accountDetails.get(username),
+                true,
+                true,
+                true,
+                true,
+                Arrays.asList(new SimpleGrantedAuthority("USER")));
+    }
+}
