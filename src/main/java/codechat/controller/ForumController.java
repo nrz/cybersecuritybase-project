ed3a4e5ae7311be3779ff1a62d89e5845ca9f098
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-28 12:12:59 +0200

    Fixes to `ForumController`.
    	modified:   src/main/java/codechat/controller/ForumController.java

--- src/main/java/codechat/controller/ForumController.java
+++ src/main/java/codechat/controller/ForumController.java
@@ -1,22 +1,34 @@
 package codechat.controller;
 
+import codechat.domain.Forum;
+import codechat.repository.ForumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForumController {
 
+    @Autowired
+    private ForumRepository forumRepository;
+
     @RequestMapping(value = "/forum/{id}", method = RequestMethod.GET)
-    public String getForum(@PathVariable Long id) {
+    public String getForum(Model model, @PathVariable Long id) {
         // Forum page.
+        Forum forum = this.forumRepository.findOne(id);
+        model.addAttribute("forum", forum);
         return "forum";
     }
 
     @RequestMapping(value = "/forum", method = RequestMethod.POST)
-    public String addForum(String forumName) {
+    public String addForum(@RequestParam String forumName) {
         // Create new forum.
+        Forum forum = new Forum(forumName);
+        this.forumRepository.save(forum);
 
         // TODO: Redirected to the newly created forum.
         return "";
