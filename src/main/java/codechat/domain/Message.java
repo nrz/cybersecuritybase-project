b242cab5b0cea6d50484d488e2c8f95eb1f79173
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-01-30 18:19:36 +0200

    Modified `public class Message`.
    `Message` contains now timestamp, a reference to a `Forum`, and a
    reference to a `Person`.
    
    	modified:   src/main/java/codechat/domain/Message.java

--- src/main/java/codechat/domain/Message.java
+++ src/main/java/codechat/domain/Message.java
@@ -1,25 +1,69 @@
 package codechat.domain;
 
+import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class Message extends AbstractPersistable<Long> {
 
-    private String messageText;
+    private String text;
+
+    @ManyToOne
+    private Person person;
+
+    @ManyToOne
+    private Forum forum;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date messageTimestamp;
 
     public Message() {
-        this.messageText = "";
+        this.messageTimestamp = new Date();
     }
 
-    public String getMessageText() {
-        if (this.messageText == null) {
-            this.messageText = "";
+    public String getText() {
+        if (this.text == null) {
+            this.text = "";
         }
-        return this.messageText;
+        return this.text;
     }
 
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Person getPerson() {
+        return this.person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Forum getForum() {
+        return this.forum;
+    }
+
+    public void setForum(Forum forum) {
+        this.forum = forum;
+    }
+
+    public Date getMessageTimestamp() {
+        return this.messageTimestamp;
+    }
+
+    public String getTimeString() {
+        if (this.messageTimestamp == null) {
+            return "";
+        }
+        return this.messageTimestamp.toString();
+    }
+
+    public void setMessageTimestamp(Date messageTimestamp) {
+        this.messageTimestamp = messageTimestamp;
     }
 }
