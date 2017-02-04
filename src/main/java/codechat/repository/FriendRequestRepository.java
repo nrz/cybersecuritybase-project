e6f39476c870ff34696c00d39a782fcfbfc23491
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-04 10:22:50 +0200

    `FriendRequestRepository`.
    	new file:   src/main/java/codechat/repository/FriendRequestRepository.java

---
+++ src/main/java/codechat/repository/FriendRequestRepository.java
@@ -0,0 +1,8 @@
+package codechat.repository;
+
+import codechat.domain.FriendRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+
+}
