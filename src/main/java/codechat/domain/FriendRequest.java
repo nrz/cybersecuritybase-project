29762388d275d03d0c61e15de79a62b3d4e17938
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-04 13:43:53 +0200

    Edited `public class FriendRequest`.
    	modified:   src/main/java/codechat/domain/FriendRequest.java

--- src/main/java/codechat/domain/FriendRequest.java
+++ src/main/java/codechat/domain/FriendRequest.java
@@ -12,4 +12,37 @@ public class FriendRequest extends AbstractPersistable<Long> {
 
     @ManyToOne
     private Person to;
+
+    private String message;
+
+    public FriendRequest() {
+
+    }
+
+    public Person getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(Person from) {
+        this.from = from;
+    }
+
+    public Person getTo() {
+        return this.to;
+    }
+
+    public void setTo(Person to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        if (this.message == null) {
+            this.message = "";
+        }
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
