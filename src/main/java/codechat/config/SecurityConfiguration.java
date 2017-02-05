832790fce45ee478b87895120c0b7bbfe91ed56e
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-02-05 12:13:12 +0200

    Vulnerability #3: A8-Cross-Site Request Forgery (CSRF).
    https://www.owasp.org/index.php/Top_10_2013-Top_10
    
    Steps to reproduce:
     1. Make sure that you have not fixed the vulnerability #1
        (A3-Cross-Site Scripting (XSS)).
     2. Clean and build project.
     3. Run project.
     4. Enter `http://127.0.0.1:8080`.
     5. Login as `user1` with password `foo`.
     6. Write in field "Friend's username" the string `user2`.
     7. Write in field "Message for friend request" the string
        `<script>window.location.href='/logout'</script>`.
        If you want an an alert box to be sure that logging out was caused
        by a malicious malicious script (XSS), you can instead write the
        following string in field "Message for friend request":
        `<script>window.location.href='/logout'; alert('you have been logged out!')</script>`.
     8. Logout by pressing the "Logout" button.
     9. Login as `user2` with password `bar`.
    10. `user2` gets immediately logged out and gets always logged
        immediately out as long as the malicious friend request remains.
    
    	modified:   src/main/java/codechat/config/SecurityConfiguration.java

--- src/main/java/codechat/config/SecurityConfiguration.java
+++ src/main/java/codechat/config/SecurityConfiguration.java
@@ -23,6 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().sameOrigin();
+        http.csrf().disable();
 
         // Admin pages only for admins.
         http.authorizeRequests()
